80dd70f1d4af25dce711c2cb86167df481b9e72c
rushikesh katkar <katkarrushikesh2002@gmail.com>
2025-01-18 17:29:06 +0530

    Script file added

---
+++ Product_Module.sql
@@ -0,0 +1,16 @@
+
+Create database ProductCatelogMVC;
+
+use ProductCatelogMVC;
+
+select * from Users;
+select * from Categories;
+select * from Products;
+
+insert into Categories values
+('Cloths', 'Cloths includes all type of clothing'),
+('Shoes', 'Shoes includes all type of Shoes');
+
+insert into Users values
+('rushi@gamil.com', 'rushikesh@gmail.com', '$2a$11$ZHPTx/zcAzZ88pvMjLOQ1uPM0iDp1tZi5r.mGkNpyo23bSrI/C5EC', 'Admin');
+
